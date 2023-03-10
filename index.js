7167727ec9ae004bbd45386b4612e4bf4a11b8c0
Harpreet Singh <hurpreet@icloud.com>
2023-03-10 17:08:02 -0500

    Learned and completed assignment about functions

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string)
+{
+return string.toUpperCase();
+}
+function whisper(string)
+{
+    return string.toLowerCase();
+}
+function logShout(string)
+{
+    console.log(string.toUpperCase());
+}
+function logWhisper(string)
+{
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string)
+{
+    const stringA= "I can't hear you!";
+    const stringB= "YES INDEED!";
+    const stringC= "I would love to!" 
+    if(string.toLowerCase(string)=== string)
+    {
+        return stringA;
+    }
+    else if(string.toUpperCase(string)=== string)
+    {
+        return stringB;
+    }
+    else if ("Let's have dinner together!"=== string)
+    return stringC;
+}
